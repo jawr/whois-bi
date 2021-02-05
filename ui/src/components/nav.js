fa1ba06fa8d63ff84c9210c32373fcc758d6f0fd
Jess Lawrence <git@lawrence.pm>
2021-02-05 18:11:31 +0900

    formatting

--- ui/src/components/nav.js
+++ ui/src/components/nav.js
@@ -5,62 +5,62 @@ import { useSelector, useDispatch } from 'react-redux'
 import { actions as loginActions } from '../store/login'
 
 export default () => {
-	const loggedIn = useSelector(state => state.login.LoggedIn)
+  const loggedIn = useSelector(state => state.login.LoggedIn)
 
-	let nav = <Anonymous />
+  let nav = <Anonymous />
 
-		if (loggedIn) nav = <User />
+    if (loggedIn) nav = <User />
 
-		return nav
+    return nav
 }
 
 const Anonymous = () => (
-	<div className="w-100 border-box pa3 ph5-l bg-white absolute top-0 left-0 right-0">
-		<Wrapper>
+  <div className="w-100 border-box pa3 ph5-l bg-white absolute top-0 left-0 right-0">
+    <Wrapper>
 
-			<nav className="db dt-l w-100">
-				<Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
-					<h3 className="f3 lh-title">
-						<span className="bg-light-green lh-copy white pa2 tracked-tight">
-							Whois.
-						</span>
-					</h3>
-				</Link>
+      <nav className="db dt-l w-100">
+        <Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
+          <h3 className="f3 lh-title">
+            <span className="bg-light-green lh-copy white pa2 tracked-tight">
+              Whois.
+            </span>
+          </h3>
+        </Link>
 
-				<div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-					<Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/register" title="Register">Register</Link>
-					<Link className="link dim dark-gray f6 f5-l dib" to="/login" title="Login">Login</Link>
-				</div>
-			</nav>
-		</Wrapper>
-	</div>
+        <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
+          <Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/register" title="Register">Register</Link>
+          <Link className="link dim dark-gray f6 f5-l dib" to="/login" title="Login">Login</Link>
+        </div>
+      </nav>
+    </Wrapper>
+  </div>
 )
 
 const User = () => {
-	const dispatch = useDispatch()
-	const handleLogout = (e) => {
-		dispatch(loginActions.logout())
-	}
+  const dispatch = useDispatch()
+  const handleLogout = (e) => {
+    dispatch(loginActions.logout())
+  }
 
-	return (
-		<div className="w-100 border-box pa3 ph5-l bg-white absolute top-0 left-0 right-0">
-			<Wrapper>
+  return (
+    <div className="w-100 border-box pa3 ph5-l bg-white absolute top-0 left-0 right-0">
+      <Wrapper>
 
-				<nav className="db dt-l w-100">
-					<Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
-						<h3 className="f3 lh-title">
-							<span className="bg-light-green lh-copy white pa2 tracked-tight">
-								Whois.
-							</span>
-						</h3>
-					</Link>
+        <nav className="db dt-l w-100">
+          <Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
+            <h3 className="f3 lh-title">
+              <span className="bg-light-green lh-copy white pa2 tracked-tight">
+                Whois.
+              </span>
+            </h3>
+          </Link>
 
-					<div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-						<Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/dashboard" title="Dashboard">Dashboard</Link>
-						<Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/" title="Logout" onClick={handleLogout}>Logout</Link>
-					</div>
-				</nav>
-			</Wrapper>
-		</div>
-	)
+          <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
+            <Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/dashboard" title="Dashboard">Dashboard</Link>
+            <Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/" title="Logout" onClick={handleLogout}>Logout</Link>
+          </div>
+        </nav>
+      </Wrapper>
+    </div>
+  )
 }
--- ui/src/components/table.js
+++ ui/src/components/table.js
@@ -4,71 +4,70 @@ import { useTable, useSortBy } from 'react-table'
 // expose more getters
 // https://codesandbox.io/s/github/tannerlinsley/react-table/tree/master/examples/data-driven-classes-and-styles
 export const Table = ({ columns, data }) => {
-	const {
-		getTableProps,
-		getTableBodyProps,
-		headerGroups,
-		rows,
-		prepareRow,
-	} = useTable(
-		{
-			columns,
-			data,
-		},
-		useSortBy
-	)
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
 
-	if (data.length === 0) {
-		return <p>No records found</p>
-	}
+  if (data.length === 0) {
+    return <p>No records found</p>
+  }
 
-	return (
-		<>
+  return (
+    <>
 
-			<table {...getTableProps()} className="f6 mw8 center dt--fixed" cellSpacing="0">
-				<thead>
-					{headerGroups.map(headerGroup => (
-						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map(column => (
-								<th {...column.getHeaderProps({
-									...column.getSortByToggleProps(),
-									className: column.headerClassName,
-								})}
-								>
-									{!column.dontSort
-										? column.isSorted
-										? column.isSortedDesc
-											? <span className="sort-by asc"></span>
-											: <span className="sort-by desc"></span>
-											: <span className="sort-by"></span>
-											: <></>
-									}
+      <table {...getTableProps()} className="f6 mw8 center dt--fixed" cellSpacing="0">
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps({
+                  ...column.getSortByToggleProps(),
+                  className: column.headerClassName,
+                })}
+                >
+                  {!column.dontSort
+                    ? column.isSorted
+                      ? column.isSortedDesc
+                        ? <span className="sort-by asc"></span>
+                        : <span className="sort-by desc"></span>
+                        : <span className="sort-by"></span>
+                        : <></>
+                  }
 
-									{column.render('Header')}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()} className="lh-copy tl">
-					{rows.map(
-						(row, i) => {
-							prepareRow(row);
-							return (
-								<tr {...row.getRowProps()}>
-									{row.cells.map(cell => {
-										return (
-											<td {...cell.getCellProps({
-												className: cell.column.cellClassName,
-											})}>{cell.render('Cell')}</td>
-										)
-									})}
-								</tr>
-							)}
-					)}
-				</tbody>
-			</table>
-		</>
-	)
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className="lh-copy tl">
+          {rows.map(
+            (row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps({
+                        className: cell.column.cellClassName,
+                      })}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+              )}
+          )}
+        </tbody>
+      </table>
+    </>
+  )
 }
-
