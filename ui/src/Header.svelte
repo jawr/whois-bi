88a4594f62b4a0666094d42a4a976583a6d80bb6
Jess Lawrence <git@lawrence.pm>
2021-03-18 10:34:56 +0900

    fix #25

--- ui/src/Header.svelte
+++ ui/src/Header.svelte
@@ -13,13 +13,13 @@
 <div class="w-100 border-box pa3 ph5-l bg-white absolute top-0 left-0 right-0">
 	<div class="mw9 center db border-box">
 		<nav class="db dt-l w-100">
-			<Link class="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
+			<span class="db dtc-l v-mid mid-gray w-100 w-25-l tc tl-l mb2 mb0-l">
 				<h3 class="f3 lh-title">
-					<span class="bg-light-green lh-copy white pa2 tracked-tight">
+					<Link to="/" title="Home" class="bg-light-green lh-copy white pa2 tracked-tight link dim">
 						Whois.
-					</span>
+					</Link>
 				</h3>
-			</Link>
+			</span>
 			<div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
 				{#if $loggedIn }
 					<Link class="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/" title="Dashboard">Dashboard</Link>
